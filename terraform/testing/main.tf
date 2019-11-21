5196bdade9c6d4f3867ee01edfc93c9e7d233019
Chin Rui Bin <52031118+Red-Flag-Pole@users.noreply.github.com>
2019-11-21 18:02:53 +0800

    Fix docker swarm playbook, add playbook for Azure File Share

--- terraform/testing/main.tf
+++ terraform/testing/main.tf
@@ -69,6 +69,30 @@ resource "azurerm_network_security_group" "netcdTestingSG" {
         destination_address_prefix = "*"
     }
 
+    security_rule {
+        name                       = "HTTPS"
+        priority                   = 1002
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "443"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    security_rule {
+        name                       = "HTTP"
+        priority                   = 1003
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
     tags = {
         environment = "NetCD Testing"
     }
